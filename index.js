95777e89ee29edb3f436e54357c85c0ed80cff56
Sourav-1234 <sourav024saha@gmail.com>
2024-10-18 16:28:29 +0530

    Created the vercel.json

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const { resolve } = require('path');
 const app = express();
 const port = 3000;
 
-app.use(express.static('static'));
+app.use(express.static('static'));z
 
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
